7090b8d7051b9e718eabecdf4eae8bc8c707eaf1
jiasr <jiasirui3@163.com>
2024-04-24 10:53:39 +0800

    update

--- mall/cmd/api.py
+++ mall/cmd/api.py
@@ -1,15 +1,17 @@
 from mall import  app
 import os
 from oslo_config import cfg
-import logging
+from oslo_log import log as logging
 
 LOG = logging.getLogger(__name__)
 CONF_FILE_PATH = os.path.join('../../resource/conf', "mall.conf")
 CONF = cfg.CONF
+logging.register_options(CONF)
 
 def load_config():
     print(CONF_FILE_PATH)
     CONF(['--config-file', CONF_FILE_PATH], project="mall")
+    logging.setup(CONF,"mall")
     CONF.log_opt_values(LOG, logging.INFO)
 
 
