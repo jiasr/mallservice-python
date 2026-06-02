1928cc627eda4ff722af31f992479ef8c20fe201
jiasr <jiasirui@inspur.com>
2026-06-02 17:56:36 +0800

    update address

--- mall/db/models/User/useraddresssql.py
+++ mall/db/models/User/useraddresssql.py
@@ -3,6 +3,10 @@ from mall.db.models.User.model import UserAddress
 import uuid
 from mall.db.engines.mysql import get_session
 from mall.common.constant import SETTING_LIST_DEFAILT_PAGESIZE
+from oslo_log import log as logging
+
+
+LOG = logging.getLogger(__name__)
 
 
 class AddressDao:
@@ -29,28 +33,48 @@ class AddressDao:
         # 'isDefault': 0,
         # 'addressTag': '',
         # 'storeId': None}}
+        if data.get("address").get("id")  is not None:
+            session = get_session()
+            with session.begin():
+                instance = session.query(UserAddress).filter(UserAddress.id ==data.get("address").get("id")).first()
+                if instance is None:
+                    LOG.info("传入id不正确")
+                else:
+                    instance.name = data.get("address").get("name")
+                    instance.name = data.get("address").get("name"),
+                    instance.mobile = data.get("address").get("phone"),
+                    instance.province = data.get("address").get("provinceName"),
+                    instance.provincecode = data.get("address").get("provinceCode"),
+                    instance.city = data.get("address").get("cityName"),
+                    instance.citycode = data.get("address").get("cityCode"),
+                    instance.district = data.get("address").get("districtName"),
+                    instance.districtcode = data.get("address").get("districtCode"),
+                    instance.detail = data.get("address").get("detailAddress"),
+                    instance.is_defalut = int(data.get("address").get("isDefault")),
+                    instance.addressTag = data.get("address").get("addressTag")
+                    LOG.info("修改完成")
 
-        session = get_session()
-        with session.begin():
-            instance = UserAddress(
-                id=uuid.uuid4().hex,
-                userid=data.get("userid"),
-                name =data.get("address").get("name"),
-                mobile=data.get("address").get("phone"),
-                province=data.get("address").get("provinceName"),
-                provincecode = data.get("address").get("provinceCode"),
-                city=data.get("address").get("cityName"),
-                citycode=data.get("address").get("cityCode"),
-                district=data.get("address").get("districtName"),
-                districtcode=data.get("address").get("districtCode"),
-                detail=data.get("address").get("detailAddress"),
-                is_defalut=int(data.get("address").get("isDefault")),
-                addressTag=data.get("address").get("addressTag")
+        else:
+            session = get_session()
+            with session.begin():
+                instance = UserAddress(
+                    id=uuid.uuid4().hex,
+                    userid=data.get("userid"),
+                    name =data.get("address").get("name"),
+                    mobile=data.get("address").get("phone"),
+                    province=data.get("address").get("provinceName"),
+                    provincecode = data.get("address").get("provinceCode"),
+                    city=data.get("address").get("cityName"),
+                    citycode=data.get("address").get("cityCode"),
+                    district=data.get("address").get("districtName"),
+                    districtcode=data.get("address").get("districtCode"),
+                    detail=data.get("address").get("detailAddress"),
+                    is_defalut=int(data.get("address").get("isDefault")),
+                    addressTag=data.get("address").get("addressTag")
 
-            )
-            session.add(instance)
-
-        return instance.id
+                )
+                session.add(instance)
+            return instance.id
 
     @classmethod
     def user_address_list(cls, params):
